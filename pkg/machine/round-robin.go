029c3263951400c9ad5cb2eab586c1e143433a46
Moleus <fafufuburr@gmail.com>
2024-01-06 21:39:55 +0300

    fix: should compare quantum with global timer instead of proc runTime

--- pkg/machine/round-robin.go
+++ pkg/machine/round-robin.go
@@ -16,7 +16,7 @@ func (r *RoundRobin) ChooseToEvict(procs []*Process) []*Process {
 		if p.runningTime > r.quantum {
 			panic(fmt.Sprintf("Process %d has passed time %d, but quantum is %d", p.id, p.runningTime, r.quantum))
 		}
-		if p.runningTime == r.quantum || p.IsTaskCompleted() {
+		if p.clock.GetCurrentTick() % r.quantum == 0 || p.IsTaskCompleted() {
 			procsToEvict = append(procsToEvict, p)
 		}
 	}

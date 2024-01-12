9a281e13aaabf83ddcdea6f08abc3b4e1e99d327
Moleus <fafufuburr@gmail.com>
2024-01-12 10:23:41 +0300

    feat: add script and results to git

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+output="./results"
+stats="./results/stats"
+variant="data/91582.txt"
+
+mkdir -p $output
+mkdir -p $stats
+
+./main -cpus 3 -algo fcfs -input $variant -log warn -output $output/fcfs.txt -procStats $stats/fcfs.stats
+./main -cpus 3 -algo rr1  -input $variant -log warn -output $output/rr1.txt  -procStats $stats/rr1.stats
+./main -cpus 3 -algo rr4  -input $variant -log warn -output $output/rr4.txt  -procStats $stats/rr4.stats
+./main -cpus 3 -algo spn  -input $variant -log warn -output $output/spn.txt  -procStats $stats/spn.stats
+./main -cpus 3 -algo srt  -input $variant -log warn -output $output/srt.txt  -procStats $stats/srt.stats
+./main -cpus 3 -algo hrrn -input $variant -log warn -output $output/hrrn.txt -procStats $stats/hrrn.stats
